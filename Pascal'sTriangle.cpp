bfcf547739c967acd734af4197c50f6682787b6a
Vivek Prakash <129273051+vivek27kgp@users.noreply.github.com>
2023-06-19 23:14:34 +0530

    2nd question solution

---
+++ Pascal'sTriangle.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> ans;
+        for (int i = 0; i < numRows; i++)
+        {
+            vector<int> level(i + 1, 1);
+            for (int j = 1; j < i; j++)
+            {
+                level[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
