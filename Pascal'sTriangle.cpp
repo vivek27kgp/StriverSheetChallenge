dff01fe5bfb44145d2c770d7ae86332098eb8fb4
Vivek Prakash <129273051+vivek27kgp@users.noreply.github.com>
2023-06-19 23:26:19 +0530

    q2

--- Pascal'sTriangle.cpp
+++ Pascal'sTriangle.cpp
@@ -1,21 +1,18 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-class Solution
+vector<vector<long long int>> printPascal(int n)
 {
-public:
-    vector<vector<int>> generate(int numRows)
+    // Write your code here.
+
+    vector<vector<long long int>> ans;
+    for (int i = 0; i < n; i++)
     {
-        vector<vector<int>> ans;
-        for (int i = 0; i < numRows; i++)
+        vector<long long int> level(i + 1, 1);
+        for (int j = 1; j < i; j++)
         {
-            vector<int> level(i + 1, 1);
-            for (int j = 1; j < i; j++)
-            {
-                level[j] = ans[i - 1][j - 1] + ans[i - 1][j];
-            }
-            ans.push_back(level);
+            level[j] = ans[i - 1][j - 1] + ans[i - 1][j];
         }
-        return ans;
+        ans.push_back(level);
     }
-};
+    return ans;
+}
