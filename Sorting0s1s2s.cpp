f9fd4ce9950b0f64c2e8b2909a17d34482e7f8ac
Vivek Prakash <129273051+vivek27kgp@users.noreply.github.com>
2023-06-20 21:49:56 +0530

    q5

---
+++ Sorting0s1s2s.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+void sort012(int *arr, int n)
+{
+    //   Write your code here
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+            cnt0++;
+        else if (arr[i] == 1)
+            cnt1++;
+        else
+            cnt2++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (cnt0 > 0)
+        {
+            arr[i] = 0;
+            cnt0--;
+        }
+        else if (cnt1 > 0)
+        {
+            arr[i] = 1;
+            cnt1--;
+        }
+        else
+        {
+            arr[i] = 2;
+            cnt2--;
+        }
+    }
+}
